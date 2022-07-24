343b24c8474f39e95016b2ce88da48254810300e
Carlos Amengual <carlosame644+github@gmail.com>
2022-07-24 11:59:36 +0200

    svgom: improve the SVGAnimatedPathData and SVGPathSeg javadocs.

--- svgom-api/src/main/java/org/w3c/dom/svg/SVGAnimatedPathData.java
+++ svgom-api/src/main/java/org/w3c/dom/svg/SVGAnimatedPathData.java
@@ -12,12 +12,94 @@
 
 package org.w3c.dom.svg;
 
+/**
+ * Implemented by elements which have a {@code d} attribute which holds SVG path
+ * data.
+ */
 public interface SVGAnimatedPathData {
+
+	/**
+	 * Gets the base contents of the {@code d} attribute.
+	 * <p>
+	 * The base (<i>i.e.</i>, static) contents of the {@code d} attribute are
+	 * provided in a form which matches one-for-one with SVG's syntax. Thus, if the
+	 * {@code d} attribute has an "absolute moveto (M)" and an "absolute arcto (A)"
+	 * command, then {@code pathSegList} will have two entries: a
+	 * {@link SVGPathSeg#PATHSEG_MOVETO_ABS PATHSEG_MOVETO_ABS} and a
+	 * {@link SVGPathSeg#PATHSEG_ARC_ABS PATHSEG_ARC_ABS}.
+	 * </p>
+	 * 
+	 * @return the base contents of the {@code d} attribute.
+	 */
 	public SVGPathSegList getPathSegList();
 
+	/**
+	 * Gets the normalized contents of the {@code d} attribute.
+	 * <p>
+	 * The base (<i>i.e.</i>, static) contents of the {@code d} attribute are
+	 * provided in a form where all path data commands are expressed in terms of the
+	 * following subset of {@link SVGPathSeg} types:
+	 * {@link SVGPathSeg#PATHSEG_MOVETO_ABS PATHSEG_MOVETO_ABS} ({@code M}),
+	 * {@link SVGPathSeg#PATHSEG_LINETO_ABS PATHSEG_LINETO_ABS} ({@code L}),
+	 * {@link SVGPathSeg#PATHSEG_CURVETO_CUBIC_ABS PATHSEG_CURVETO_CUBIC_ABS}
+	 * ({@code C}) and {@link SVGPathSeg#PATHSEG_CLOSEPATH PATHSEG_CLOSEPATH}
+	 * ({@code z}).
+	 * </p>
+	 * <p>
+	 * Thus, if the {@code d} attribute has an "absolute moveto (M)" and an
+	 * "absolute arcto (A)" command, then {@code normalizedpathSegList} will have
+	 * one {@code PATHSEG_MOVETO_ABS} entry followed by a series of
+	 * {@code PATHSEG_LINETO_ABS} entries which approximate the arc. This alternate
+	 * representation is available to provide a simpler interface to developers who
+	 * would benefit from a more limited set of commands.
+	 * </p>
+	 * <p>
+	 * The only valid {@code SVGPathSeg} types are {@code PATHSEG_MOVETO_ABS}
+	 * ({@code M}), {@code PATHSEG_LINETO_ABS} ({@code L}),
+	 * {@code PATHSEG_CURVETO_CUBIC_ABS} ({@code C}) and {@code PATHSEG_CLOSEPATH}
+	 * ({@code z}).
+	 * </p>
+	 * 
+	 * @return the normalized contents of the {@code d} attribute.
+	 */
 	public SVGPathSegList getNormalizedPathSegList();
 
+	/**
+	 * Gets the current animated contents of the {@code d} attribute.
+	 * <p>
+	 * The current animated contents of the {@code d} attribute are provided in a
+	 * form which matches one-for-one with SVG's syntax. If the given attribute or
+	 * property is being animated, contains the current animated value of the
+	 * attribute or property, and both the object itself and its contents are read
+	 * only. If the given attribute or property is not currently being animated,
+	 * gives the same value as {@link #getPathSegList()}.
+	 * </p>
+	 * 
+	 * @return the current animated contents of the {@code d} attribute.
+	 */
 	public SVGPathSegList getAnimatedPathSegList();
 
+	/**
+	 * Gets the normalized current animated contents of the {@code d} attribute.
+	 * <p>
+	 * The current animated contents of the {@code d} attribute are provided in a
+	 * form where all path data commands are expressed in terms of the following
+	 * subset of {@link SVGPathSeg} types: {@link SVGPathSeg#PATHSEG_MOVETO_ABS
+	 * PATHSEG_MOVETO_ABS} ({@code M}), {@link SVGPathSeg#PATHSEG_LINETO_ABS
+	 * PATHSEG_LINETO_ABS} ({@code L}), {@link SVGPathSeg#PATHSEG_CURVETO_CUBIC_ABS
+	 * PATHSEG_CURVETO_CUBIC_ABS} ({@code C}) and
+	 * {@link SVGPathSeg#PATHSEG_CLOSEPATH PATHSEG_CLOSEPATH} ({@code z}).
+	 * </p>
+	 * <p>
+	 * If the given attribute or property is being animated, contains the current
+	 * animated value of the attribute or property, and both the object itself and
+	 * its contents are read only. If the given attribute or property is not
+	 * currently being animated, contains the same value as
+	 * {@link #getNormalizedPathSegList()}.
+	 * </p>
+	 * 
+	 * @return the normalized current animated contents of the {@code d} attribute.
+	 */
 	public SVGPathSegList getAnimatedNormalizedPathSegList();
+
 }
